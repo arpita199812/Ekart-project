1515e75a07cbb3864b60bfc7502a736f4f18eade
Arpita Singh <158558110+arpita199812@users.noreply.github.com>
2024-09-10 13:26:00 +0530

    Update ShoppingCartApplication.java

--- src/main/java/com/reljicd/ShoppingCartApplication.java
+++ src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -2,11 +2,19 @@ package com.reljicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.method.annotation.WebMvcRegistrations;
+import org.springframework.web.servlet.mvc.method.annotation.WebMvcRegistrationsAdapter;
 
 @SpringBootApplication
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcRegistrations webMvcRegistrations() {
+        return new WebMvcRegistrationsAdapter();
     }
 }
